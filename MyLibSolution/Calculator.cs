f1133191e1afe0ad8228a7a4313e3cd2a8d1835d
MKudryash <jukudryash@gmail.com>
2026-03-23 13:07:27 +0300

    init lib

---
+++ MyLibSolution/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace MyLibSolution;
+
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+    public double Divide(double a, double b)
+    {
+        if (b == 0) throw new DivideByZeroException();
+        return a / b;
+    }
+}
